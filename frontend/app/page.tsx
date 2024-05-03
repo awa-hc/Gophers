eda9d2d356d5acb49f05bf7e33043f017b1a1ef2
Horacio Cortez <leohermoso18@gmail.com>
2024-05-03 16:31:33 -0300

    initial commit for vercel
    initial commit for vercel

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Index } from "@/components/Index";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Index/>
+    </div>
+  );
+}
