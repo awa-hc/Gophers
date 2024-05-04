b09fd2587d6169f6d0b7c08a9ddbdb51ef2d0c7e
Horacio Cortez <leohermoso18@gmail.com>
2024-05-04 13:11:07 -0300

    feat: Update button text in Index component to schedule a meeting

--- frontend/components/Index.tsx
+++ frontend/components/Index.tsx
@@ -485,9 +485,9 @@ export function Index() {
           </p>
           <TypewriterEffectSmooth words={words} />
           <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-            <Link href="/contact" passHref>
+            <Link href="/meet" passHref>
               <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
-                Join now
+                Schedule a meeting
               </button>
             </Link>
             <Link href="/contact" passHref>
